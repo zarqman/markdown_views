c8090de8f94dc5a213a22854e54a51206c5c4b4c
thomas morgan <tm@iprog.com>
2017-05-22 17:25:14 -0600

    allow indenting of blocks for markdown() helper

--- app/helpers/markdown_views_helper.rb
+++ app/helpers/markdown_views_helper.rb
@@ -5,8 +5,29 @@ module MarkdownViewsHelper
       source = capture do
         yield
       end
+      source = unindent source
     end
     MarkdownViews::Renderer.render(source)
+  end
+
+  # unindents a block of text according to the least indented line
+  def unindent(source)
+    spaces = 100
+    lines = source.split("\n")
+    lines.each do |line|
+      if line =~ /[^\s]/
+        line =~ /^(\s*)/
+        spaces = [spaces, $1.length].min
+      end
+      break if spaces.zero?
+    end
+    if spaces > 0 && spaces < 100
+      lines.each do |line|
+        line.sub! /^\s{#{spaces}}/, ''
+      end
+      source = lines.join("\n")
+    end
+    source
   end
 
 end
