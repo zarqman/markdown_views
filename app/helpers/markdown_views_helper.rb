9ee0aa588c179637c82dd7cc965c2248984765f4
thomas morgan <tm@iprog.com>
2017-07-07 15:23:12 -0600

    replace unindent() with comparable activesupport method

--- app/helpers/markdown_views_helper.rb
+++ app/helpers/markdown_views_helper.rb
@@ -4,30 +4,9 @@ module MarkdownViewsHelper
     if block_given?
       source = capture do
         yield
-      end
-      source = unindent source
+      end.strip_heredoc
     end
     MarkdownViews::Renderer.render(source)
-  end
-
-  # unindents a block of text according to the least indented line
-  def unindent(source)
-    spaces = 100
-    lines = source.split("\n")
-    lines.each do |line|
-      if line =~ /[^\s]/
-        line =~ /^(\s*)/
-        spaces = [spaces, $1.length].min
-      end
-      break if spaces.zero?
-    end
-    if spaces > 0 && spaces < 100
-      lines.each do |line|
-        line.sub! /^\s{#{spaces}}/, ''
-      end
-      source = lines.join("\n")
-    end
-    source
   end
 
 end
