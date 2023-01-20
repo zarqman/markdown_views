3aca387c2cb9dc7dba6e7bd61bfb50ac1de37573
thomas morgan <tm@iprog.com>
2023-01-19 22:30:56 -0700

    keep helpers when action_controller.include_all_helpers=false

--- lib/markdown_views/engine.rb
+++ lib/markdown_views/engine.rb
@@ -1,4 +1,14 @@
 module MarkdownViews
   class Engine < ::Rails::Engine
+    config.autoload_once_paths = %W(
+      #{root}/app/helpers
+    )
+
+    initializer "markdown_views.helpers", before: :load_config_initializers do
+      ActiveSupport.on_load(:action_controller_base) do
+        helper MarkdownViews::Engine.helpers
+      end
+    end
+
   end
 end
--- lib/markdown_views/version.rb
+++ lib/markdown_views/version.rb
@@ -1,3 +1,3 @@
 module MarkdownViews
-  VERSION = '2.2.5'
+  VERSION = '2.3.0'
 end
